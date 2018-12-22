18033cb63ed771a0539c45eda471a87faeb2fe9a
ykstr <yannickuester@gmail.com>
2018-12-22 16:38:11 +0100

    Added class skeletons Added data structure fields required for the planned code-image process pipeline

---
+++ java/src/de/ykstr/jpiet/models/PietInterpreter.java
@@ -0,0 +1,4 @@
+package de.ykstr.jpiet.models;
+
+public class PietInterpreter {
+}
---
+++ java/src/de/ykstr/jpiet/models/PietProgram.java
@@ -0,0 +1,11 @@
+package de.ykstr.jpiet.models;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class PietProgram {
+    private BufferedImage sourceImage;
+    private Color[][] codels;
+    private ArrayList<Codel> blocks;
+}
