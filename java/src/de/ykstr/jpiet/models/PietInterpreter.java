eede228a5ef5dbd40361a4dd0d7df95f978ff9ab
ykstr <yannickuester@gmail.com>
2018-12-23 13:51:49 +0100

    Added direction pointer and codel chooser to the Piet interpreter

--- java/src/de/ykstr/jpiet/models/PietInterpreter.java
+++ java/src/de/ykstr/jpiet/models/PietInterpreter.java
@@ -1,4 +1,10 @@
 package de.ykstr.jpiet.models;
 
+import de.ykstr.jpiet.enums.Direction;
+
 public class PietInterpreter {
+    //Direction Pointer
+    Direction DP;
+    //Codel Chooser
+    Direction CC;
 }
