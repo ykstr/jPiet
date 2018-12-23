6af5bd8303a9f823db948e9443b078d80d11e035
ykstr <yannickuester@gmail.com>
2018-12-23 13:54:13 +0100

    Added an App class containing the main method for test executions (and later an executable interpreter program from the command line)

---
+++ java/src/de/ykstr/jpiet/App.java
@@ -0,0 +1,32 @@
+package de.ykstr.jpiet;
+
+import de.ykstr.jpiet.enums.CodedColor;
+import de.ykstr.jpiet.models.PietProgram;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) {
+        File f = new File(args[0]);
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PietProgram p = new PietProgram(image,PietProgram.findScalar(image));
+        writeImage("./data/piet",p.toImage());
+    }
+
+    public static void writeImage(String path, BufferedImage image){
+        File f = new File(path+".gif");
+        try {
+            ImageIO.write(image, "gif",f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
